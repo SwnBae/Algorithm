0b3845928a863c4aa1a2195a472c962150abc500
SwnBae <128581113+SwnBae@users.noreply.github.com>
2025-02-13 09:51:28 +0900

    [Silver II] Title: 외판원 순회 2, Time: 552 ms, Memory: 17068 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+// 가지치기를 통해 구현해보자, 애초에 경우의 수 제외
 public class Main {
 	
 	public static int n;
@@ -10,31 +10,17 @@ public class Main {
 	public static boolean[] visited;
 	public static int min = Integer.MAX_VALUE;
 	
-	public static void dfs(int cnt) {
+	public static void dfs(int cnt, int prev) {
 		if(cnt == n) {
+			if(cost[channel[n - 1]][channel[0]] == 0) { // 못 가는 경우
+				return;
+			}
+			
 			int tmpCost = 0;
 			
 			for(int i = 0; i < n; i++) {
-				if(cost[channel[i % n]][channel[(i+1) % n]] == 0) { // 못 가는 경우
-					//System.out.println(channel[i % n] + "," + channel[(i+1) % n]);
-					return;
-				}
-				
 				tmpCost += cost[channel[i % n]][channel[(i+1) % n]];
 			}
-			
-//			for(int i = 0; i < n - 1; i++) {
-//				if(cost[channel[i]][channel[i+1]] == 0) { // 못 가는 경우
-//					return;
-//				}
-//				tmpCost += cost[channel[i]][channel[i+1]];
-//			}
-//			
-//			if (cost[channel[n - 1]][channel[0]] == 0) {
-//			    return;
-//			}
-//			
-//			tmpCost += cost[channel[n-1]][channel[0]];
 			
 			
 			min = Math.min(min, tmpCost);
@@ -43,11 +29,11 @@ public class Main {
 		}
 		
 		for(int i = 0; i < n; i++) {
-			if(visited[i]) continue;
+			if(visited[i] || (cost[prev][i+1] == 0)) continue;
 			
 			visited[i] = true;
 			channel[cnt] = i + 1;
-			dfs(cnt + 1);
+			dfs(cnt + 1,channel[cnt]);
 			visited[i] = false;
 		}
 	}
@@ -68,7 +54,12 @@ public class Main {
 			}
 		}
 		
-		dfs(0);
+		for(int i = 0; i <=n; i++) {
+			cost[0][i] = -1;
+			cost[i][0] = -1;
+		}
+		
+		dfs(0,0);
 		
 		System.out.println(min);
 
