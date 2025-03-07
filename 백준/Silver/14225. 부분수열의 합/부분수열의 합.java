c526565e7d231392a947275087487ae97efa03cd
SwnBae <128581113+SwnBae@users.noreply.github.com>
2025-03-07 12:00:55 +0900

    [Silver I] Title: 부분수열의 합, Time: 132 ms, Memory: 16412 KB -BaekjoonHub

--- 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
@@ -2,47 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static Set<Integer> set;
+	public static boolean[] visited;
 	public static int n;
 	public static int[] arr;
 	
 	public static void dfs(int cnt, int sum) {
 		if(cnt == n) {
-			set.add(sum);
+			visited[sum] = true;
 			return;
 		}
 		
 		dfs(cnt + 1, sum);
 		dfs(cnt + 1, sum + arr[cnt]);
-		
 	}
 	
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        set = new TreeSet<>();
+       visited = new boolean[100000 * 20 + 1];
         
         n = Integer.parseInt(br.readLine());
         arr = Arrays.stream(br.readLine().split(" "))
         		.mapToInt((a) -> Integer.parseInt(a))
         		.toArray();
         
-        
         dfs(0,0);
         
-        Iterator<Integer> iterator = set.iterator();
-        
-        int pre = iterator.next();
-        
-        for(int num : set) {
-        	if(num - pre > 1) {
-        		System.out.println(pre + 1);
-        		return;
-        	}
+        for(int i = 0; i < visited.length; i++) {
+        	if(visited[i]) continue;
         	
-        	pre = num;
+        	System.out.println(i);
+        	return;
         }
-        
-        System.out.println(pre + 1);
     }
 }
