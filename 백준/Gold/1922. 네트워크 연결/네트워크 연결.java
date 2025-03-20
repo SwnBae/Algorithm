959dce2aa578e4ccb540517faf5f1beb5b11ad59
SwnBae <128581113+SwnBae@users.noreply.github.com>
2025-03-20 11:18:55 +0900

    [Gold IV] Title: 네트워크 연결, Time: 540 ms, Memory: 57208 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -0,0 +1,96 @@
+import java.io.*;
+import java.util.*;
+
+//크루스칼
+//유니온 파인드 구현하기
+//pq 사용하기
+class Edge{
+	int start;
+	int end;
+	int cost;
+	
+	public Edge(int start, int end, int cost) {
+		this.start = start;
+		this.end = end;
+		this.cost = cost;
+	}
+}
+
+public class Main {
+	public static PriorityQueue<Edge> pq;
+	public static int[] parent;
+	public static int[] rank;
+	
+	public static int n;
+	public static int m;
+	
+	public static int find(int node) {
+		if(node != parent[node]) {
+			parent[node] = find(parent[node]);
+		}
+		
+		return parent[node];
+	}
+	
+	public static boolean union(int a, int b) {
+		a = find(a);
+		b = find(b);
+		
+		if(a == b) return false;
+		
+		if(rank[a] > rank[b]) {
+			parent[b] = a;
+			rank[a] += rank[b];
+		} else {
+			parent[a] = b;
+			rank[b] += a;
+		}
+		
+		return true;
+	}
+	
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int result = 0;
+		
+		n = Integer.parseInt(br.readLine());
+		m = Integer.parseInt(br.readLine());
+		
+		parent = new int[n+1];
+		rank = new int[n+1];
+		
+		pq = new PriorityQueue<>(new Comparator<Edge>() {
+			@Override
+			public int compare(Edge o1, Edge o2) {
+				return Integer.compare(o1.cost, o2.cost);
+			}
+		});
+		
+		for(int i = 0 ; i <= n; i++) {
+			parent[i] = i;
+			rank[i] = 1;
+		}
+		
+		for(int i = 0; i < m; i++) {
+			String[] input = br.readLine().split(" ");
+			
+			int a = Integer.parseInt(input[0]);
+			int b = Integer.parseInt(input[1]);
+			int c = Integer.parseInt(input[2]);
+			
+			pq.add(new Edge(a,b,c));
+		}
+		
+		while(!pq.isEmpty()) {
+			Edge edge = pq.poll();
+			
+			if(union(edge.start, edge.end)) {
+				result += edge.cost;
+			}
+		}
+		
+		System.out.println(result);
+	}
+}
