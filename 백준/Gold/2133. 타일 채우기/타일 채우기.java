5ac9c20538c2dc3a74abe39cc061976a164396ab
SwnBae <128581113+SwnBae@users.noreply.github.com>
2025-04-02 10:48:08 +0900

    [Gold IV] Title: 타일 채우기, Time: 104 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.java
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -4,47 +4,21 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int n = Integer.parseInt(br.readLine());
-
-		int[][] dp = new int[n + 1][4];
-
-		dp[1][0] = 0;
-
-		if (n > 1) {
-			dp[2][1] = 1;
-			dp[2][2] = 1;
-			dp[2][3] = 1;
-			dp[2][0] = 3;
-			
-			
-		}
 		
-		if (n > 2) {
-			dp[2][1] = 1;
-			dp[2][2] = 1;
-			dp[2][3] = 1;
-			dp[2][0] = 3;
-			
-			dp[3][1] = 1;
-			dp[3][3] = 1;
-		}
-
-		for (int i = 4; i <= n; i++) {
-			if (i % 2 == 0) {
-				dp[i][1] = dp[i - 2][1] + dp[i - 2][2] + dp[i - 2][3] + dp[i - 1][3];
-				dp[i][2] = dp[i - 2][1] + dp[i - 2][2] + dp[i - 2][3];
-				dp[i][3] = dp[i][1];
-
-				dp[i][0] = dp[i][1] + dp[i][2] + dp[i][3];
-			} else {
-				dp[i][1] = dp[i - 1][3];
-				dp[i][3] = dp[i - 1][1];
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] dp = new int[31];
+		
+		dp[1] = 0;
+		dp[2] = 3;
+		
+		for(int i = 4; i <= n; i+=2) {
+			dp[i] = dp[i-2] * 3 + 2;
+			for(int j = 4; j <= i; j+=2) {
+				dp[i] += 2 * dp[i-j];
 			}
 		}
 		
-		System.out.println(dp[n][0]);
-
+		System.out.println(dp[n]);
 	}
-
 }
