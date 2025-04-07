96b12255284b282d02fa061b21a5641e5d9de63d
SwnBae <128581113+SwnBae@users.noreply.github.com>
2025-04-07 22:33:37 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 780 ms, Memory: 210528 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -0,0 +1,117 @@
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+
+public class Main {
+    public static int[] dy = {0, 1, 1};
+    public static int[] dx = {1, 1, 0};
+    public static int n;
+    public static int[][] space;
+    public static int[][] count;
+
+    public static boolean outBound(int y, int x){
+        return y < 0 || y >= n || x < 0 || x >= n;
+    }
+
+    public static boolean checkCross(int y, int x){
+        for(int i = 0; i < 3; i++){
+            int tmpY = y + dy[i];
+            int tmpX = x + dx[i];
+
+            if(outBound(tmpY,tmpX) || space[tmpY][tmpX] == 1) return true;
+        }
+
+        return false;
+    }
+
+    public static int bfs(){
+        ArrayDeque<int[]> queue = new ArrayDeque<>();
+
+        queue.add(new int[]{0,1,0});
+
+        while (!queue.isEmpty()){
+            int[] tmp = queue.poll();
+            int y = tmp[0];
+            int x = tmp[1];
+            int preDir = tmp[2];
+
+
+
+            if(preDir == 0){
+                for(int i = 0; i < 3; i++){
+                    if(i == 2) continue;
+
+                    if(i == 1){
+                        if(checkCross(y,x)) continue;
+                    }
+                    int tmpY = y + dy[i];
+                    int tmpX = x + dx[i];
+
+                    if(outBound(tmpY,tmpX) || space[tmpY][tmpX] == 1) continue;
+
+                    count[tmpY][tmpX]++;
+
+                    queue.add(new int[]{tmpY,tmpX,i});
+                }
+            } else if(preDir == 2){
+                for(int i = 0; i < 3; i++){
+                    if(i == 0) continue;
+
+                    if(i == 1){
+                        if(checkCross(y,x)) continue;
+                    }
+                    int tmpY = y + dy[i];
+                    int tmpX = x + dx[i];
+
+                    if(outBound(tmpY,tmpX) || space[tmpY][tmpX] == 1) continue;
+
+                    count[tmpY][tmpX]++;
+
+                    queue.add(new int[]{tmpY,tmpX,i});
+                }
+            } else{
+                for(int i = 0; i < 3; i++){
+                    int tmpY = y + dy[i];
+                    int tmpX = x + dx[i];
+
+                    if(i == 1){
+                        if(checkCross(y,x)) continue;
+                    }
+
+                    if(outBound(tmpY,tmpX) || space[tmpY][tmpX] == 1) continue;
+
+                    count[tmpY][tmpX]++;
+
+                    queue.add(new int[]{tmpY,tmpX, i});
+                }
+            }
+
+        }
+
+        return count[n-1][n-1];
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        space = new int[n][n];
+        count = new int[n][n];
+
+        for(int i = 0; i < n; i++){
+            String[] input = br.readLine().split(" ");
+            for(int j = 0; j < n; j++){
+                space[i][j] = Integer.parseInt(input[j]);
+            }
+        }
+
+        if(space[n-1][n-1] == 1){
+            System.out.println(0);
+            return;
+        }
+
+        System.out.println(bfs());
+
+    }
+}
