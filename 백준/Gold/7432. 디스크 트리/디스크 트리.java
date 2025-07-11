19fe82b06d95fdb0a5b26d8998042898149d1531
SwnBae <128581113+SwnBae@users.noreply.github.com>
2025-07-11 13:31:32 +0900

    [Gold III] Title: 디스크 트리, Time: 232 ms, Memory: 24520 KB -BaekjoonHub

---
+++ 백준/Gold/7432. 디스크 트리/디스크 트리.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class Node{
+        Map<String, Node> child = new TreeMap<>();
+        boolean isEnd;
+    }
+
+    static class Trie{
+        Node root;
+
+        Trie(){
+            this.root = new Node();
+        }
+
+        void insert(String[] arr){
+            Node node = this.root;
+
+            for(int i = 0; i < arr.length; i++){
+                String str = arr[i];
+
+                if(!node.child.containsKey(str)){
+                    node.child.put(str, new Node());
+                }
+
+                node = node.child.get(str);
+            }
+
+            node.isEnd = true;
+        }
+    }
+
+    public static StringBuilder sb;
+
+    public static void dfs(Node node, int depth){
+        for(String str : node.child.keySet()){
+            sb.append(" ".repeat(depth));
+            sb.append(str + "\n");
+            dfs(node.child.get(str), depth + 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        sb = new StringBuilder();
+        Trie trie = new Trie();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++){
+            trie.insert(br.readLine().split("\\\\"));
+        }
+
+        dfs(trie.root, 0);
+
+        System.out.println(sb.toString());
+    }
+}
