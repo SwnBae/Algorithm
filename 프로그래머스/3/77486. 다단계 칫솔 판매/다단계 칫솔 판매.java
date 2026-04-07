a2b22940ce05a43e495ef39d9047202186f75e96
SwnBae <128581113+SwnBae@users.noreply.github.com>
2026-04-07 18:51:03 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 4765.10 ms, Memory: 122 MB -BaekjoonHub

---
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    int[] parents;
+    Map<String, Integer> nameIdx;
+    int[] answer;
+    
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        answer = new int[enroll.length];
+        parents = new int[enroll.length + 1];
+        nameIdx = new HashMap<>();
+        
+        int count = 1;
+        
+        for(int i = 1; i <= enroll.length; i++) {
+            nameIdx.put(enroll[i - 1], i);
+        }
+        
+        for(int i = 1; i <= enroll.length; i++) {
+            if(referral[i - 1].equals("-")) continue;
+            
+            parents[i] = nameIdx.get(referral[i - 1]);
+        }
+        
+        for(int i = 0; i < seller.length; i++) {
+            calculate(nameIdx.get(seller[i]), amount[i] * 100);
+        }
+        
+        return answer;
+    }
+    
+    public void calculate(int idx, int amount) {
+        int curAmount = amount;
+        int curIdx = idx;
+        
+        while(curIdx != 0) {
+            answer[curIdx - 1] += curAmount - ((int) (curAmount * 0.1));
+            curAmount = (int) (curAmount * 0.1);
+            
+            curIdx = parents[curIdx];
+        }
+    }
+}
