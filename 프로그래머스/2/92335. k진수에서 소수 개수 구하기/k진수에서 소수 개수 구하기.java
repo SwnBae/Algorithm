edcbeca88d9f2a1c864315273e41d1f74f797c8c
SwnBae <128581113+SwnBae@users.noreply.github.com>
2026-05-08 20:52:44 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 3.55 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        String num = Integer.toString(n, k);
+        String[] nums = num.split("0");
+        
+        // 최댓값 구하기
+        long max = 0;
+        for (String number : nums) {
+            if (number.equals("")) continue;
+            long val = Long.parseLong(number);
+            if (val > max) max = val;
+        }
+        
+        if (max < 2) return 0;
+        
+        // max가 배열로 만들기 너무 크면 에라토스테네스 못 씀
+        // 근데 문제 제한상 그냥 소수 판별 함수로 처리
+        for (String number : nums) {
+            if (number.equals("")) continue;
+            long val = Long.parseLong(number);
+            if (isPrime(val)) answer++;
+        }
+        
+        return answer;
+    }
+    
+    private boolean isPrime(long n) {
+        if (n < 2) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+        for (long i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
